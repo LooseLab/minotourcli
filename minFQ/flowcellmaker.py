7493ccedfe8230b3f9fc6a4b614c24c2519a885c
Matt Loose <mattloose@DestroyerOfWorlds.local>
2019-02-03 22:46:00 +0000

    cleaning up uneccessary print statements

--- minFQ/flowcellmaker.py
+++ minFQ/flowcellmaker.py
@@ -66,10 +66,8 @@ def main():
     for root, subdirs, files in tqdm(os.walk(args.watchdir)):
         files.sort()
         for file in tqdm(files):
-            #print (file)
             if file.endswith("fast5"):
                 if file.split('_read_')[0] != last_seen:
-                    #print (last_seen,file.split('_read_')[0])
                     last_seen = file.split('_read_')[0]
                     try:
                         try:
@@ -78,18 +76,12 @@ def main():
                         except:
                             mymultifile = multi_fast5.MultiFast5File(os.path.join(root,file))
                             myid = mymultifile.get_read_ids()[0]
-                            myfile = mymultifile.get_read(myid)
-
-                        #print (myfile)
-                        #print (myfile.get_tracking_id())
+                            myfile = mymultifile.get_read(myid))
                         minIONid = myfile.get_tracking_id()['device_id']
                         minION.add(minIONid)
                         flowcell = myfile.get_tracking_id()['flow_cell_id']
-                        print ("seen flowcell:{}".format(flowcell))
                         if len(flowcell) < 1:
-                            print ("Flowcell ID missing from file {}.".format(file))
                             flowcell = input('Enter a flowcell name: ')
-                            
                         sample_id = myfile.get_tracking_id()['sample_id']
                         run_id = myfile.get_tracking_id()['run_id']
                         if sample_id not in flowcelldict.keys():
@@ -102,55 +94,28 @@ def main():
                             flowcelldict[sample_id][flowcell][run_id]["context_tags"]=myfile.get_context_tags()
                     except:
                         print ("Non fast5file seen.")
-
-                #else:
-                    #print ("same read")
-
-                #print (myfile.get_context_tags())
-
-
-    #print (minION)
-    #print (flowcelldict)
-
     print ("Adding new runs.")
-
     full_host = "http://{}:{}/".format(args.host_name, str(args.port_number))
-
-
     header = {
         'Authorization': 'Token {}'.format(args.api_key),
         'Content-Type': 'application/json'
     }
-
     minotourapi = MinotourAPI(full_host, header)
-
-
     for minIONs in minION:
-        #print ("Create {}".format(minIONs))
         minion = minotourapi.get_minion_by_name(minIONs)
         if not minion:
             minion = minotourapi.create_minion(minIONs)
         minion = minion
-
-#    print (flowcelldict)
-
     for sample_id in tqdm(flowcelldict):
-#        print ("sample_id:{}".format(sample_id))
         for flowcellid in flowcelldict[sample_id].keys():
-            print ("Looking for {}".format(flowcellid))
             flowcell = minotourapi.get_flowcell_by_name(flowcellid)
-            #print ("found flowcell {}".format(flowcell))
             if len(flowcell["data"])==0:
-                print ("Not found Flowcell")
                 flowcell = minotourapi.create_flowcell(flowcellid)
-
             for run_id in flowcelldict[sample_id][flowcellid]:
-                #print (run_id)
                 run = minotourapi.get_run_by_runid(run_id)
                 if not run:
                     is_barcoded = False  # TODO do we known this info at this moment? This can be determined from run info.
                     has_fastq = True  # TODO do we known this info at this moment? This can be determined from run info
-
                     run = minotourapi.create_run(sample_id, run_id, is_barcoded, has_fastq,
                                                             flowcell, minion,
                                                             flowcelldict[sample_id][flowcellid][run_id]['tracking_id']['exp_start_time'])
@@ -171,16 +136,9 @@ def main():
                     "minKNOW_real_sample_rate": int(str(flowcelldict[sample_id][flowcellid][run_id]['context_tags']['sample_frequency'])),
                     "minKNOW_asic_id": flowcelldict[sample_id][flowcellid][run_id]['tracking_id']['asic_id'],
                     "minKNOW_start_time": flowcelldict[sample_id][flowcellid][run_id]['tracking_id']['exp_start_time'],
-                    # "minKNOW_colours_string": str(self.rpc_connection.analysis_configuration.get_channel_states_desc()),
-                    #"minKNOW_colours_string": str(
-                    #    MessageToJson(self.rpc_connection.analysis_configuration.get_channel_states_desc(),
-                    #                  preserving_proto_field_name=True, including_default_value_fields=True)),
                     "minKNOW_computer": str(flowcelldict[sample_id][flowcellid][run_id]['tracking_id']['hostname']),
                 }
                 payload['flowcell_type']=str(flowcelldict[sample_id][flowcellid][run_id]['context_tags']['flowcell_type'])
                 payload['sequencing_kit']=str(flowcelldict[sample_id][flowcellid][run_id]['context_tags']['sequencing_kit'])
-                #print (run)
-                updateruninfo = minotourapi.update_minion_run_info(payload, run['id'])
-                #print (updateruninfo)
 
     print ("Metadata Uploaded to MinoTour.")
